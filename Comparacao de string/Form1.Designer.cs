081265ca2a865d0a15d1fbadb47a723e6b4aa3dc
taveirajc <taveirajc@gmail.com>
2021-05-23 19:03:58 -0300

    Adicionar arquivos de projeto.

---
+++ Comparacao de string/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Comparacao_de_string
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Compara = new System.Windows.Forms.Button();
+            this.btn_Sair = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Compara
+            // 
+            this.btn_Compara.Location = new System.Drawing.Point(279, 141);
+            this.btn_Compara.Name = "btn_Compara";
+            this.btn_Compara.Size = new System.Drawing.Size(75, 23);
+            this.btn_Compara.TabIndex = 0;
+            this.btn_Compara.Text = "Comparar";
+            this.btn_Compara.UseVisualStyleBackColor = true;
+            this.btn_Compara.Click += new System.EventHandler(this.btn_Compara_Click);
+            // 
+            // btn_Sair
+            // 
+            this.btn_Sair.Location = new System.Drawing.Point(546, 141);
+            this.btn_Sair.Name = "btn_Sair";
+            this.btn_Sair.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sair.TabIndex = 1;
+            this.btn_Sair.Text = "Sair";
+            this.btn_Sair.UseVisualStyleBackColor = true;
+            this.btn_Sair.Click += new System.EventHandler(this.btn_Sair_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(471, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(215, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(471, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(212, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "String 1 - Digite";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "String 2 - Digite";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Sienna;
+            this.label3.Location = new System.Drawing.Point(109, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.DarkOrange;
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(190, 135);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Escolha um método:";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoCheck = false;
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(7, 26);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(160, 19);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Método 1 - String Equals";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoCheck = false;
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 49);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(173, 19);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Método 2 - String.Compare";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoCheck = false;
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 72);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(152, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Método 3 - CompareTo";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoCheck = false;
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 95);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(174, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Método 4 - StringComparer";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Location = new System.Drawing.Point(403, 141);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(75, 23);
+            this.btn_limpar.TabIndex = 8;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(19, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Mensagem:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(709, 237);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Sair);
+            this.Controls.Add(this.btn_Compara);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Métodos de comparação de strings";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Compara;
+        private System.Windows.Forms.Button btn_Sair;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Comparacao de string/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Comparacao_de_string
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.AutoCheck = true; // isso para iniciar o form com os radiobuttons desmarcados.
+            radioButton2.AutoCheck = true; // selecionar todos os radiobuttons no design e marcar a proriedade AutoCheck para False.
+            radioButton3.AutoCheck = true;
+            radioButton4.AutoCheck = true;
+            textBox1.Select();
+            textBox1.Focus();
+
+        }
+
+
+        // rotina para verificar o radio buttom selecionado. Retorna o índice que começa com zero
+        public int getCheckedRadioButton(Control c)
+        {
+            int i, index = -1;
+            Control.ControlCollection cc = c.Controls;
+            for (i = 0; i < cc.Count; i++)
+            {
+                if (cc[i] is RadioButton)
+                {
+                    index++;
+                    RadioButton rb = cc[i] as RadioButton;
+                    if (rb.Checked)
+                    {
+                        return index;  //retorna o indice do RadioButton selecionado
+                    }
+                }
+            }
+            
+            return -1;
+        }
+
+        // rotina para comparar
+        private void btn_Compara_Click(object sender, EventArgs e)
+        {
+                 int ver = getCheckedRadioButton(groupBox1); // chama a função que retorna o índice do RadioButton selecionado e guarda em ver
+                 if (ver == -1)
+                 {
+                     System.Console.Beep();
+                     label3.Text = "Favor selecionar um método.";
+                 }
+                 else
+                     if ((textBox1.Text == string.Empty || textBox2.Text == string.Empty) || (textBox1.Text == string.Empty && textBox2.Text == string.Empty))
+                     {
+                         System.Console.Beep();
+                         label3.Text = "Favor preencher as strings a serem comparadas.";
+                         //textBox1.Text = ver.ToString(); // SE QUISESSE RETORNAR O NOME SERIA DESSA FORMA.
+                     }
+                     else
+                     {
+                         switch (ver)
+                         {
+                             case 0:
+                                 System.Console.Beep();
+                                 // Compara as strings usando String.Equals  
+                                 if (String.Equals(textBox1.Text, textBox2.Text))
+                                     label3.Text = "As strings são iguais.";
+                                 else
+                                     label3.Text = "As strings são diferentes.";
+                                 break;
+
+                             case 1:
+                                 System.Console.Beep();
+                                 // Usando o método String.Compare
+                                 if (String.Compare(textBox1.Text, textBox2.Text) == 0)
+                                     label3.Text = "As strings são iguais.";
+                                 else if (String.Compare(textBox1.Text, textBox2.Text) < 0)
+                                     label3.Text = textBox1.Text + " vem antes de " + textBox2.Text;
+                                 else if (String.Compare(textBox1.Text, textBox2.Text) > 0)
+                                     label3.Text = textBox1.Text + " vem depois de " + textBox2.Text;
+                                 break;
+
+                             case 2:
+                                 System.Console.Beep();
+                                 // Usando o método CompareTo
+                                 if (textBox1.Text.CompareTo(textBox2.Text) == 0)
+                                     label3.Text = "As strings são iguais.";
+                                 else if (textBox1.Text.CompareTo(textBox2.Text) < 0)
+                                     label3.Text = textBox1.Text + " vem antes de " + textBox2.Text;
+                                 else if (textBox1.Text.CompareTo(textBox2.Text) > 0)
+                                     label3.Text = textBox1.Text + " vem depois de " + textBox2.Text;
+                                 break;
+
+                             case 3:
+                                 System.Console.Beep();
+                                 //Usando StringComparer  
+                                 StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+                                 if (comparer.Compare(textBox1.Text, textBox2.Text) == 0)
+                                     label3.Text = "As strings são iguais.";
+                                 else if (comparer.Compare(textBox1.Text, textBox2.Text) < 0)
+                                     label3.Text = textBox1.Text + " vem antes de " + textBox2.Text;
+                                 else if (comparer.Compare(textBox1.Text, textBox2.Text) > 0)
+                                     label3.Text = textBox1.Text + " vem depois de " + textBox2.Text;
+                                 break;
+
+                         }
+                     }
+        }
+
+
+        // Rotina que limpa os controles
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            label3.Text = string.Empty;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+        }
+
+        private void btn_Sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
